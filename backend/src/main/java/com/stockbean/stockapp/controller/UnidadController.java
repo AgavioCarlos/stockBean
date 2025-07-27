4f4334063e608fa5478f65d651d730a0cef27903
AgavioCar <agaviocarlos@gmail.com>
2025-07-26 23:37:40 -0600

    add: Se agregó el modelo, repositorio, servicio y controlador de la tabla Unidad

---
+++ backend/src/main/java/com/stockbean/stockapp/controller/UnidadController.java
@@ -0,0 +1,49 @@
+package com.stockbean.stockapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stockbean.stockapp.model.catalogos.Unidad;
+import com.stockbean.stockapp.service.UnidadService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("/unidades")
+public class UnidadController {
+    @Autowired 
+    private UnidadService unidadService;
+
+
+    @GetMapping
+    public List<Unidad> listar() {
+        return unidadService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Unidad> obtener(@PathVariable Integer id){
+        Unidad unidades = unidadService.obtenerPorId(id);
+        return unidades != null ? ResponseEntity.ok(unidades) : ResponseEntity.notFound().build();
+    } 
+
+    @PostMapping()
+    public Unidad crear(@RequestBody Unidad unidades){
+        return unidadService.guardar(unidades);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Unidad> actualizar(@PathVariable Integer id, @RequestBody Unidad unidades) {
+        Unidad actualizado = unidadService.actualizar(id, unidades);
+        return actualizado != null ? ResponseEntity.ok(actualizado) : ResponseEntity.notFound().build();
+    }
+}
