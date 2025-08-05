bd5734e3fd23f5cde6e6b69873600fcc9679e048
AgavioCar <agaviocarlos@gmail.com>
2025-08-04 21:23:55 -0600

    add: Se agregó la pagina de Productos y Compras

---
+++ backend/src/main/java/com/stockbean/stockapp/model/tablas/Producto.java
@@ -0,0 +1,35 @@
+package com.stockbean.stockapp.model.tablas;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tbl_productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_producto;
+    private String nombre;
+    private String descripcion;
+    private Integer id_categoria;
+    private Integer id_unidad;
+    private Integer id_marca;
+    private String codigo_barras;
+    private String imagen_url;
+    private Boolean status;
+
+    private LocalDateTime fecha_alta;
+    private LocalDateTime fecha_baja;
+    private LocalDateTime fecha_ultima_modificacion;
+}
