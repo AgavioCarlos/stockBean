b91ff5281c87e20e8ccd2f309ac1cf500d94dc7b
AgavioCar <agaviocarlos@gmail.com>
2025-11-17 11:48:10 -0600

    update: Se actualizarón los componentes y la configuración de NodeJS

--- frontend/w-stockapp/src/main.tsx
+++ frontend/w-stockapp/src/main.tsx
@@ -1,11 +1,116 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-/* import App from './App.tsx' */
 import './index.css'
-import Login from './pages/Login/Login'
+import Persona from './pages/Persona/Persona'
+import Home from './pages/Home'
+import Login from './pages/Login/Login';
+import Perfil from './pages/Perfil';
+import Categorias from './pages/Categorias';
+import Roles from './pages/Roles';
+import Inicio from './pages/Inicio';
+import Productos from './pages/Productos'
+import Marcas from './pages/Marcas'
+import Clientes from './pages/Clientes'
+import Proveedores from './pages/Proveedores'
+import PuntoVenta from './pages/PuntoVenta'
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Inicio/>
+    ),
+  },
+  {
+    path: "/home",
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/perfil",
+    element: (
+      <ProtectedRoute>
+        <Perfil />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+    {
+    path: "/productos",
+    element: (
+      <Productos/>
+    ),
+  },
+  {
+    path: "/persona",
+    element: (
+      <ProtectedRoute>
+        <Persona />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/categorias",
+    element: (
+      <ProtectedRoute>
+        <Categorias />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/roles",
+    element: (
+      <ProtectedRoute>
+        <Roles />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/marcas",
+    element: (
+      <ProtectedRoute>
+        <Marcas/>
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/clientes",
+    element: (
+      <ProtectedRoute>
+        <Clientes/>
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/proveedores",
+    element: (
+      <ProtectedRoute>
+        <Proveedores/>
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/punto-venta",
+    element: (
+      <ProtectedRoute>
+        <PuntoVenta/>
+      </ProtectedRoute>
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Login />
+    {/* <Login />
+    <Persona /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
-)
+);
