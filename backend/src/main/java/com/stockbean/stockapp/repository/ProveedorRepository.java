216ecad17e6c1b6d0f5195c1cdb0446b2655f62c
AgavioCar <agaviocarlos@gmail.com>
2025-11-17 11:20:32 -0600

    add: Se agregó el repositorio y servicio Proveedor

---
+++ backend/src/main/java/com/stockbean/stockapp/repository/ProveedorRepository.java
@@ -0,0 +1,12 @@
+package com.stockbean.stockapp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.stockbean.stockapp.model.tablas.Producto;
+import com.stockbean.stockapp.model.tablas.Proveedor;
+
+public interface ProveedorRepository extends JpaRepository<Proveedor, Integer>{
+    List<Producto> findByStatus(Boolean status);
+}
