d8bd4c0235f2e22410c22642382d248a3f159d4d
Carlos Agavio <agaviocarlos@gmail.com>
2026-02-09 15:53:50 -0600

    Update: Se actualizaron los repositorios

--- backend/src/main/java/com/stockbean/stockapp/repository/EmpresaUsuarioRepository.java
+++ backend/src/main/java/com/stockbean/stockapp/repository/EmpresaUsuarioRepository.java
@@ -1,10 +1,31 @@
 package com.stockbean.stockapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.stockbean.stockapp.dto.EmpresaUsuarioDTO;
 import com.stockbean.stockapp.model.admin.EmpresaUsuario;
 
 @Repository
 public interface EmpresaUsuarioRepository extends JpaRepository<EmpresaUsuario, Integer> {
+
+    @Query("""
+            SELECT new com.stockbean.stockapp.dto.EmpresaUsuarioDTO(
+                            em.nombreComercial,
+                            us.cuenta,
+                            CONCAT(pe.nombre, ' ', pe.apellido_paterno, ' ', pe.apellido_materno)
+            )
+            FROM EmpresaUsuario eu
+            INNER JOIN eu.usuario us
+            INNER JOIN us.persona pe
+            INNER JOIN eu.empresa em
+            WHERE eu.usuario.id_usuario = :id_usuario
+            """)
+
+    List<EmpresaUsuarioDTO> validarEmpresaUsuario(@Param("id_usuario") Integer id_usuario);
 
 }
---
+++ backend/src/main/java/com/stockbean/stockapp/repository/PantallaRepository.java
@@ -0,0 +1,29 @@
+package com.stockbean.stockapp.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.stockbean.stockapp.dto.PantallaDTO;
+import com.stockbean.stockapp.model.admin.Pantallas;
+
+public interface PantallaRepository extends JpaRepository<Pantallas, Integer> {
+    @Query("""
+            SELECT new com.stockbean.stockapp.dto.PantallaDTO(
+                pt.idPantalla,
+                pt.clave,
+                pt.nombre,
+                pt.ruta,
+                pt.icono,
+                pt.orden,
+                pt.idPadre,
+                pt.esMenu
+            )
+            FROM RolPermisos rp
+            INNER JOIN PantallasPermisos pp ON pp.permiso.idPermiso = rp.permiso.idPermiso
+            INNER JOIN pp.pantalla pt
+            WHERE rp.rol.id_rol = :idRol
+            """)
+    List<PantallaDTO> findByIdPantalla(@Param("idRol") Integer idRol);
+
+}
