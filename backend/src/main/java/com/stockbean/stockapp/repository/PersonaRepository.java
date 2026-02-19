b55dcf3d6097445ba9ccb1fd12e9eb17c7d2d3f8
Carlos Agavio <agaviocarlos@gmail.com>
2026-02-19 09:56:56 -0600

    Update: Se actualizó el modelo Sucursal y repositorios

--- backend/src/main/java/com/stockbean/stockapp/repository/PersonaRepository.java
+++ backend/src/main/java/com/stockbean/stockapp/repository/PersonaRepository.java
@@ -4,7 +4,11 @@ import com.stockbean.stockapp.model.tablas.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface PersonaRepository extends JpaRepository<Persona, Integer>{
-    List<Persona> findByStatus(Boolean status);   
+public interface PersonaRepository extends JpaRepository<Persona, Integer> {
+    List<Persona> findByStatus(Boolean status);
+
     boolean existsByEmail(String email);
+
+    @org.springframework.data.jpa.repository.Query("SELECT p FROM EmpresaUsuario eu JOIN eu.usuario u JOIN u.persona p WHERE eu.empresa.idEmpresa = :idEmpresa")
+    List<Persona> findByEmpresaId(@org.springframework.data.repository.query.Param("idEmpresa") Integer idEmpresa);
 }
